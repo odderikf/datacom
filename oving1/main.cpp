27f349182e88453932bffaa8f6932a10cdc4eb24
odderikf <odderikf@stud.ntnu.no>
2019-02-07 13:05:56 +0100

    Moved to better distribution of work

--- oving1/main.cpp
+++ oving1/main.cpp
@@ -8,38 +8,42 @@
 void findPrime( // fairly naìve iterative method. For every number, check divisibility for all numbers below square root.
         unsigned long long start,
         unsigned long long stop,
+        unsigned long long step,
         std::unique_ptr<std::vector<unsigned long long>>& results
         ){
     results = std::make_unique<std::vector<unsigned long long>>();
-    for(unsigned long long cur_num = start; cur_num < stop; ++cur_num){
-        for(auto i = static_cast<unsigned long long int>(std::sqrt(cur_num)); i > 1; --i){
+    for(unsigned long long cur_num = start; cur_num < stop; cur_num += step){
+        bool is_prime = cur_num % 2 == 1;
+        auto max = static_cast<unsigned long long int>(std::sqrt(cur_num));
+        if (max % 2 == 1) ++max;
+        for(unsigned long long i = 3; i <= max; i += 1){
             if(cur_num % i == 0){
-                goto outer;
+                is_prime = false;
+                break;
             }
         }
-        results->push_back(cur_num);
-        outer:;
+        if (is_prime) results->push_back(cur_num);
     }
 }
 
 int main(int argc, char* argv[]) {
-    unsigned long long start = 2;
-    unsigned long long stop = 10000000;
+    unsigned long long start = 1;
+    unsigned long long stop = 100000;
     unsigned short threadCount = 4;
     switch (argc) {
         case 1:
             break;
         case 2:
-            stop = std::stoi(argv[1]);
+            stop = static_cast<unsigned long long int>(std::stoi(argv[1]));
             break;
         case 4:
-            threadCount = std::stoi(argv[3]);
-            start = std::stoi(argv[1]);
-            stop = std::stoi(argv[2]);
+            threadCount = static_cast<unsigned short>(std::stoi(argv[3]));
+            start = static_cast<unsigned long long int>(std::stoi(argv[1]));
+            stop = static_cast<unsigned long long int>(std::stoi(argv[2]));
             break;
         case 3:
-            start = std::stoi(argv[1]);
-            stop = std::stoi(argv[2]);
+            start = static_cast<unsigned long long int>(std::stoll(argv[1]));
+            stop = static_cast<unsigned long long int>(std::stoll(argv[2]));
             break;
 
         default: // > 4
@@ -48,31 +52,25 @@ int main(int argc, char* argv[]) {
             break;
     }
 
+    std::vector<unsigned long long> final;
+    if(2 >= start){
+        final.emplace_back(2);
+        start = 3;
+    }
     std::vector<std::thread> threads;
     std::vector<std::unique_ptr<std::vector<unsigned long long>>> results(threadCount);
 
-    unsigned long remaining = stop-start;
-    double frac = 2./3.;
-
-    for(unsigned short i = 0; i < std::size(results)-1; ++i){
-        unsigned long long substart = stop - remaining;
-        auto substop = static_cast<unsigned long long int>(substart + frac * remaining);
-        remaining -= static_cast<unsigned long long int>(frac*remaining);
-        //std::cout << substart << " " << substop << std::endl;
-        threads.emplace_back(findPrime, substart, substop, std::ref(results[i]));
+    for(unsigned short i = 0; i < threadCount; ++i){
+        threads.emplace_back(findPrime, start + 2*i, stop, 2*threadCount, std::ref(results[i]));
     }
-
-    //std::cout << stop-remaining << " " << stop << std::endl;
-    threads.emplace_back(findPrime, stop - remaining, stop, std::ref(results[threadCount-1]));
     for(auto &i : threads) i.join();
 
-    std::vector<unsigned long long> final;
     for(auto &i : results){
         final.insert(std::end(final), std::begin(*i), std::end(*i));
     }
     std::sort(final.begin(), final.end());
     for(auto &i : final){
-        //std::cout << i << std::endl;
+        std::cout << i << std::endl;
     }
 
     return 0;
